eb78e7accac89c1d391af06342d95b12510fffe1
gerbaux valentin <valentin.gerbaux@etudiant.univ-reims.fr>
2026-01-14 14:42:38 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", fontFamily: "sans-serif" }}>
+      <h1>📺 Mon Tracker de Séries</h1>
+      <p>Bienvenue sur mon clone de Letterboxd.</p>
+    </div>
+  );
+}
